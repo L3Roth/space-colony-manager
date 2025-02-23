6563c6912d0223109f8512d5a67b6ee39ecc3e56
Alexander Noack <alexander.noack95@outlook.de>
2025-02-23 21:54:03 +0100

    edited the index of the ressouces lib so all parts get exported

--- libs/resources/src/index.ts
+++ libs/resources/src/index.ts
@@ -1,1 +1,5 @@
-export * from './lib/resources/resources.component';
+export * from './lib/state/resources.reducer';
+export * from './lib/state/resources.actions';
+export * from './lib/state/resources.selectors';
+export * from './lib/state/resources.state';
+
