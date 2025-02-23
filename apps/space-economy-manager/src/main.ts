5cd60557877965cbd686f3609d84f0491300aa47
Alexander Noack <alexander.noack95@outlook.de>
2025-02-23 21:54:22 +0100

    included the store inside the main.ts

--- apps/space-economy-manager/src/main.ts
+++ apps/space-economy-manager/src/main.ts
@@ -3,9 +3,12 @@ import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
+import { provideStore } from '@ngrx/store';
+import { resourcesReducer } from '@space-economy-manager/resources';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideStore({ resources: resourcesReducer }),
     provideRouter(appRoutes),
     provideHttpClient(),
   ],
