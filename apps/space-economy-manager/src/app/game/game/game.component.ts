a859b0aafade6ae59b5f7eccdd5b5bb01f1709d2
Alexander Noack <alexander.noack95@outlook.de>
2025-02-23 21:54:57 +0100

    fetch data from the store inside the main component

--- apps/space-economy-manager/src/app/game/game/game.component.ts
+++ apps/space-economy-manager/src/app/game/game/game.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameService } from '../../services/game.service';
+import { Store } from '@ngrx/store';
+import { selectFood, selectWater, selectEnergy, ResourcesState } from '@space-economy-manager/resources';
 
 @Component({
   selector: 'app-game',
-  standalone: true, // 🚀 Wichtig für Standalone Components!
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css',
 })
-export class GameComponent {
-  constructor(private gameService: GameService) {}
+export class GameComponent implements OnInit {
+  food$;
+  water$;
+  energy$;
+
+  constructor(private gameService: GameService, private store: Store<ResourcesState>) {
+    this.food$ = this.store.select(selectFood);
+    this.water$ = this.store.select(selectWater);
+    this.energy$ = this.store.select(selectEnergy);
+  }
 
   ngOnInit(): void {
     this.gameService.startGame();
