c243222e21d3cbb3e6f3479d77745b2b3081f6f1
Alexander Noack <alexander.noack95@outlook.de>
2025-02-24 15:47:26 +0100

    included the stores in the game

--- apps/space-economy-manager/src/app/game/game/game.component.ts
+++ apps/space-economy-manager/src/app/game/game/game.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { GameService } from '../../services/game.service';
 import { Store } from '@ngrx/store';
 import { selectFood, selectWater, selectEnergy, ResourcesState } from '@space-economy-manager/resources';
+import { BuildingsState, selectAllBuildings } from '@space-economy-manager/buildings';
+import { EventsState, selectAllEvents } from '@space-economy-manager/events';
+import { ColonistsState, selectAllColonists } from '@space-economy-manager/colonists';
 
 @Component({
   selector: 'app-game',
@@ -16,10 +19,28 @@ export class GameComponent implements OnInit {
   water$;
   energy$;
 
-  constructor(private gameService: GameService, private store: Store<ResourcesState>) {
-    this.food$ = this.store.select(selectFood);
-    this.water$ = this.store.select(selectWater);
-    this.energy$ = this.store.select(selectEnergy);
+  buildings$;
+
+  events$;
+
+  colonists$;
+
+  constructor(
+    private gameService: GameService,
+    private resourcesStore: Store<ResourcesState>,
+    private buildingsStore: Store<BuildingsState>,
+    private eventStore: Store<EventsState>,
+    private colonistsStore: Store<ColonistsState>
+  ) {
+    this.food$ = this.resourcesStore.select(selectFood);
+    this.water$ = this.resourcesStore.select(selectWater);
+    this.energy$ = this.resourcesStore.select(selectEnergy);
+
+    this.buildings$ = this.buildingsStore.select(selectAllBuildings);
+
+    this.events$ = this.eventStore.select(selectAllEvents);
+
+    this.colonists$ = this.colonistsStore.select(selectAllColonists);
   }
 
   ngOnInit(): void {
