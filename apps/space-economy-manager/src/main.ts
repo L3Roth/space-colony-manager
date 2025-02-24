ccae5e1ddfe606fbfaec4d648796cb148524eb02
Alexander Noack <alexander.noack95@outlook.de>
2025-02-24 13:38:41 +0100

    included the event state in the main ts

--- apps/space-economy-manager/src/main.ts
+++ apps/space-economy-manager/src/main.ts
@@ -3,15 +3,17 @@ import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { resourcesReducer } from '@space-economy-manager/resources';
 import { buildingsReducer } from '@space-economy-manager/buildings';
-
+import { eventsReducer } from '@space-economy-manager/events';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideStore({ resources: resourcesReducer }),
-    provideStore({ buildings: buildingsReducer }),
+    provideStore(),
+    provideState('resources', resourcesReducer),
+    provideState('buildings', buildingsReducer),
+    provideState('events', eventsReducer),
     provideRouter(appRoutes),
     provideHttpClient(),
   ],
