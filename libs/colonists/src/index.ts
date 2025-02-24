35a8fdb77e9b7461c2edcf942d4af3e29521e8f1
Alexander Noack <alexander.noack95@outlook.de>
2025-02-24 15:39:40 +0100

    made sure ngrx parts gets exported by editing index.ts of the colonists lib

--- libs/colonists/src/index.ts
+++ libs/colonists/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/colonists/colonists.component';
+export * from './lib/state/colonists.reducer';
+export * from './lib/state/colonists.actions';
+export * from './lib/state/colonists.selectors';
+export * from './lib/state/colonists.state';
