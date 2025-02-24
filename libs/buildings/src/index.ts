17ed5512c77aec95f6ddd1ee83b78183fd1fe3a9
Alexander Noack <alexander.noack95@outlook.de>
2025-02-24 12:42:55 +0100

    edited index of buildings lib so all ngrx parts get exported

--- libs/buildings/src/index.ts
+++ libs/buildings/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/buildings/buildings.component';
+export * from './lib/state/buildings.reducer';
+export * from './lib/state/buildings.selectors';
+export * from './lib/state/buildings.actions';
+export * from './lib/state/buildings.state';
