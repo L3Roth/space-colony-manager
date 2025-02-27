9eb153050249f7dbca59bc7b58bd76d02264830b
Alexander Noack <alexander.noack95@outlook.de>
2025-02-27 22:33:59 +0100

    edited building state for avaiable buildings

--- libs/buildings/src/lib/state/buildings.state.ts
+++ libs/buildings/src/lib/state/buildings.state.ts
@@ -13,3 +13,10 @@ export interface BuildingsState {
 export const initialBuildingsState: BuildingsState = {
   buildings: [],
 }
+
+export const avaiableBuildings: Omit<Building, 'id' | 'level' | 'isActive'>[] = [
+  { type: 'Farm', requiredColonists: 2 },
+  { type: 'Solar Plant', requiredColonists: 1 },
+  { type: 'Water Pump', requiredColonists: 3 },
+  { type: 'Habitat', requiredColonists: 0 },
+]
